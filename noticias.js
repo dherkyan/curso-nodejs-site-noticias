627201f82aed91a68146b448e2c774fa6c925861
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-05 14:11:32 -0300

    Configurando o servidor HTTP e acessando via navegador

--- noticias.js
+++ noticias.js
@@ -1,1 +1,9 @@
-console.log("Criando um site de notícias com NodeJS");
+console.log("Iniciando Portal de Notícias");
+
+var http = require('http');
+
+var server = http.createServer(function(req,res){
+    res.end("<html><body>Portal de Notícias</body></html>");
+});
+
+server.listen(3000);
