b91dc0a745d37221c9c36daba4a27567f68e4ab0
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-05 14:25:44 -0300

    Respondendo requisições com base na url

--- noticias.js
+++ noticias.js
@@ -3,7 +3,19 @@ console.log("Iniciando Portal de Notícias");
 var http = require('http');
 
 var server = http.createServer(function(req,res){
-    res.end("<html><body>Portal de Notícias</body></html>");
+
+    var categoria = req.url;
+
+    if(categoria == '/tecnologia'){
+        res.end("<html><body>Notícias  - Tecnologias</body></html>");
+    }else if(categoria == '/moda'){
+        res.end("<html><body>Notícias  - Moda</body></html>");
+    }else if(categoria == '/beleza'){
+        res.end("<html><body>Notícias  - Beleza</body></html>");
+    }else{
+        res.end("<html><body>Portal de Notícias</body></html>");
+    }
+    
 });
 
 server.listen(3000);
