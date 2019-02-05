51e4c41869d718d07bbca2fda9c6c24247870572
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-05 17:50:37 -0300

    Refactoring Conexão - Criando módulo de conexão com BD

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,15 +1,10 @@
+var conexaoManager = require('../../config/databases');
+
 module.exports = function (app) {
 
+    var conexao = conexaoManager();
+
     app.get('/noticias', function (req, res) {
-
-        var mysql = require('mysql');
-
-        var conexao = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'root',
-            database: 'portal_noticias'
-        });
 
         conexao.query('select * from noticias', function(error, result) {
             res.render("noticias/noticias" , {noticias : result});
