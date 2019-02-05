de650c785e500d2214bef6191e1e58084b98d68c
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-05 15:26:58 -0300

    Configurando EJS como View Engine e criando modulo views

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+app.set('view engine','ejs');
+
 app.get('/',function(req, res){
     res.send("<html><body>Portal de Notícias</body></html>");
 });
 
 app.get('/tecnologia',function(req, res){
-    res.send("<html><body>Notícias  - Tecnologias</body></html>");
+    res.render("secao/tecnologia");
 });
 
 app.listen(3000, function(){
