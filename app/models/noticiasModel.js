6ed5637b02a1df8d08d2e5d43d30c15d4d08f127
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-08 13:29:49 -0300

    Refactoring - Implementando classes

--- app/models/noticiasModel.js
+++ app/models/noticiasModel.js
@@ -1,16 +1,19 @@
+function Noticias(){
+
+}
+
+Noticias.prototype.getNoticias = function(conexao, callback){
+    conexao.query('SELECT * FROM NOTICIAS',callback);
+}
+
+Noticias.prototype.getNoticia = function(conexao, callback){
+    conexao.query('SELECT * FROM NOTICIAS WHERE ID_NOTICIAS = 2',callback);
+}
+
+Noticias.prototype.salvarNoticia = function(noticia, conexao, callback){
+    conexao.query('INSERT INTO NOTICIAS SET ?', noticia, callback);
+}
+
 module.exports = function(){
-    
-    this.getNoticias = function(conexao, callback){
-        conexao.query('SELECT * FROM NOTICIAS',callback);
-    }
-
-    this.getNoticia = function(conexao, callback){
-        conexao.query('SELECT * FROM NOTICIAS WHERE ID_NOTICIAS = 2',callback);
-    }
-
-    this.salvarNoticia = function(noticia, conexao, callback){
-        conexao.query('INSERT INTO NOTICIAS SET ?', noticia, callback);
-    }
-    
-    return this;
+    return Noticias;
 }
--- app/routes/admin.js
+++ app/routes/admin.js
@@ -8,7 +8,7 @@ module.exports = function(app){
         var noticia = req.body;
 
         var conexao = app.config.databases();
-        var noticiasModel = app.app.models.noticiasModel;
+        var noticiasModel = new app.app.models.noticiasModel;
 
         noticiasModel.salvarNoticia(noticia, conexao, function(error, result) {
             res.redirect("/noticias");
