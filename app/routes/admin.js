5258b417ecc6d6fd935fecf2e2faa42a7ebda773
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-12 12:48:10 -0300

    Exibindo erros de validação do formulário

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -1,32 +1,37 @@
-module.exports = function(app){
-    app.get('/formulario_inclusao_noticia',function(req, res){
-        res.render("admin/form_add_noticia");
+module.exports = function (app) {
+    app.get('/formulario_inclusao_noticia', function (req, res) {
+        res.render("admin/form_add_noticia", { validacao: {} });
     });
 
 
-    app.post('/noticias/salvar',function(req, res){
+    app.post('/noticias/salvar', function (req, res) {
+        console.log('admin.js - /noticias/salvar');
         var noticia = req.body;
 
-        req.assert('titulo','Título é obrigatório').notEmpty();
-        req.assert('resumo','Resumo é obrigatório').notEmpty();
-        req.assert('resumo','Resumo deve conter entre 10 e 100 caracteres').len(10,100);
-        req.assert('data_noticia','Data é obrigatório').notEmpty();
-        req.assert('texto','Texto da notícia é obrigatório').notEmpty();
+        req.assert('titulo', 'Título é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo é obrigatório').notEmpty();
+        req.assert('resumo', 'Resumo deve conter entre 10 e 100 caracteres').len(10, 100);
+        req.assert('data_noticia', 'Data é obrigatório').notEmpty();
+        req.assert('texto', 'Texto da notícia é obrigatório').notEmpty();
 
-        var isErrors = req.validationErrors();
+        var errors = req.validationErrors();
 
-        if(isErrors){
-            res.render('admin/form_add_noticia');
+        console.log(errors);
+
+        if (errors) {
+            res.render('admin/form_add_noticia', { validacao : errors});
             return;
         }
+        
+        console.log('salvar no bd...');
 
         var conexao = app.config.databases();
         var noticiaDao = new app.app.models.NoticiaDao(conexao);
 
-        noticiaDao.salvarNoticia(noticia, function(error, result) {
+        noticiaDao.salvarNoticia(noticia, function (error, result) {
             res.redirect("/noticias");
             //res.send(result);
         });
-
+        
     });
 }
