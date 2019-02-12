1887ebb4100894fbac53884be96e8a2d08ee2f8f
Dherkyan Ribeiro <37579@tjmt.jus.br>
2019-02-12 15:30:44 -0300

    Adcionando o tempplate clean-blog

--- config/server.js
+++ config/server.js
@@ -9,6 +9,7 @@ var app = express();
 app.set('view engine','ejs');
 app.set('views','./app/views');
 
+app.use(express.static('./app/public'));
 app.use(bodyparser.urlencoded({extended:true}));
 app.use(expressValidator());
 
